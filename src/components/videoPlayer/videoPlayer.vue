3f89df4db09f8ff8e76067398b6766add6b60f31
yz <1540032876@qq.com>
2020-12-21 10:37:05 +0800

    2020-12-21 播放器逐帧播放 yz todo 播放器进度条，音量条及相关按钮 warning 第一次刷新点击播放，前1s会有卡顿，之后重新播放bug消失

--- src/components/videoPlayer/videoPlayer.vue
+++ src/components/videoPlayer/videoPlayer.vue
@@ -1,14 +1,21 @@
 <template>
     <!--    warning: v-cloak 此指令可以解决使用插值表达式页面闪烁问题-->
-    <div class="videoPlayer" v-cloak>
-        <div>
-            <!--            <video class="player" ref="player" src="http://10.1.71.155/static/video/test.mp4"></video>-->
-            <video class="player" ref="player" src="@/assets/video/test.mp4" width="100%" height="100%"
-                   preload="auto" @canplay="videoDuration">
+    <div class="videoPlayer">
+        <div class="video_content">
+            <!--http://10.1.71.155/static/video/test.mp4-->
+            <!--@/assets/video/test.mp4-->
+            <video class="player" ref="player" src="http://10.1.71.155/static/video/test.mp4" width="100%" height="100%"
+                   preload="auto" @canplaythrough="videoCanplaythrough" @playing="videoPlaying" @pause="videoPause"
+                   @ended="videoEnd">
             </video>
         </div>
-        <a-icon v-show="!ifPlay" class="playBtn" title="播放" type="play-circle" @click="playerEvent('play')"/>
-        <a-icon v-show="ifPlay" class="pauseBtn" title="暂停" type="pause-circle" @click="playerEvent('pause')"/>
+        <div v-if="ifLoad" class="video_btn">
+            <a-icon v-show="!ifPlay" class="playerBtn" title="播放" type="play-circle" @click="playerEvent('play')"/>
+            <a-icon v-show="ifPlay" class="playerBtn" title="暂停" type="pause-circle" @click="playerEvent('pause')"/>
+            <a-icon class="playerBtn" title="停止" type="undo" @click="playerEvent('stop')"/>
+            <span class="time" ref="time">00:00:00:00</span>
+            <button title="下一帧" @click="btnClick">test next frame</button>
+        </div>
     </div>
 </template>
 
@@ -18,22 +25,52 @@
         data() {
             return {
                 player: null,
-                ifPlay: false
+                ifLoad: false,
+                ifPlay: false,
+                timer: null,
+                frameRate: 25
             }
-        },
-        props: {
-            videoUrl: String
         },
         mounted() {
             this.player = this.$refs.player
         },
         methods: {
-            videoDuration() {
+            //warning 设置定时器让播放器静音播放1s解决刷新进页面第一次播放前1s卡顿的问题
+            videoCanplaythrough() {
                 //视频加载完成
-                console.log('load')
+                if (!this.ifLoad) {
+                    this.$refs.player.muted = true
+                    this.$refs.player.play()
+                    setTimeout(() => {
+                        this.$refs.player.currentTime = 0
+                        this.$refs.player.pause()
+                        this.$refs.player.muted = false
+                        this.ifLoad = true
+                    }, 1000)
+                }
+            },
+            videoPlaying() {
+                //判断播放器正在播放
+                if (this.ifLoad) {
+                    this.timer = setInterval(() => {
+                        this.$refs.time.innerText = timeFormat(this.$refs.player.currentTime, this.frameRate)
+                    }, this.frameRate)
+                }
+            },
+            videoPause() {
+                //播放器暂停
+                clearInterval(this.timer)
+                this.timer = null
+            },
+            videoEnd() {
+                //视频播放完成
+                console.log('end')
+                this.ifPlay = false
+                this.player.currentTime = 0
+                this.$refs.time.innerText = timeFormat(this.$refs.player.currentTime, this.frameRate)
             },
             playerEvent(status) {
-                //todo 播放，暂停频繁快速切换会报错
+                //播放器 播放，暂停，停止
                 switch (status) {
                     case 'play':
                         this.ifPlay = true
@@ -45,32 +82,71 @@
                         break
                     case 'stop':
                         this.player.currentTime = 0
+                        this.ifPlay = false
                         this.player.pause()
+                        this.$refs.time.innerText = timeFormat(this.$refs.player.currentTime, this.frameRate)
                         break
                 }
+            },
+            btnClick() {
+                this.$refs.player.currentTime += 1 / 25
+                this.$refs.time.innerText = timeFormat(this.$refs.player.currentTime, this.frameRate)
             }
         }
+    }
+
+    //时分秒帧转换
+    function timeFormat(time, frameRate) {
+        let timeStr = ''
+        let stringFormat = (i) => {
+            //if i<10 return '0'+i else return i
+            return i < 10 ? `0${i}` : `${i}`
+        }
+        let stringFrame = (i) => {
+            let res = parseInt(i * frameRate)
+            return res < 10 ? `0${res}` : `${res}`
+        }
+        let minuteTime = 0
+        let secondTime = 0
+        let hourTime = 0
+        let setTime = parseInt(time)
+        let setFrame = time - parseInt(time)
+        if (setTime < 60) {
+            timeStr = `00:00:${stringFormat(setTime)}:${stringFrame(setFrame)}`
+        } else if (setTime >= 60 && setTime < 3600) {
+            minuteTime = parseInt(setTime / 60)
+            secondTime = parseInt(setTime % 60)
+            timeStr = `00:${stringFormat(minuteTime)}:${stringFormat(secondTime)}:${stringFrame(setFrame)}`
+        } else if (setTime >= 3600) {
+            let _t = parseInt(setTime % 3600)
+            hourTime = parseInt(setTime / 3600)
+            minuteTime = parseInt(_t / 60)
+            secondTime = parseInt(_t % 60)
+            timeStr = `${stringFormat(hourTime)}:${stringFormat(minuteTime)}:${stringFormat(secondTime)}:${stringFrame(setFrame)}`
+        }
+        return timeStr
     }
 </script>
 
 <style scoped lang="less">
     .videoPlayer {
+        @icon-color: rgba(0, 0, 0, 1);
         /*background: red;*/
         width: 100%;
         height: 100%;
 
-        [v-cloak] {
-            display: none;
+        .playerBtn {
+            color: @icon-color;
+            font-size: 30px;
+            padding: 5px;
         }
 
-        .playBtn {
-            color: white;
-            font-size: 30px;
-        }
-
-        .pauseBtn {
-            color: white;
-            font-size: 30px;
+        .time {
+            color: @icon-color;
+            font-size: 25px;
+            font-weight: lighter;
+            line-height: 25px;
+            padding: 5px;
         }
     }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="home">
-      <div class="home_video">
-          <videoPlayer :video-url="videoUrl"></videoPlayer>
-      </div>
+        <div class="home_video">
+            <videoPlayer></videoPlayer>
+        </div>
     </div>
 </template>
 
@@ -11,10 +11,8 @@
 
     export default {
         name: 'Home',
-        data(){
-            return {
-                videoUrl:"http://47.98.131.223/video/test.mp4"
-            }
+        data() {
+            return {}
         },
         components: {
             videoPlayer
@@ -23,9 +21,10 @@
 </script>
 
 <style scoped lang="less">
-    .home{
-        .home_video{
-            width: 500px;
+    .home {
+        .home_video {
+            margin: 0 auto;
+            width: 900px;
         }
     }
 </style>
