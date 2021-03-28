973742ac7824bac6e8ef3453749b7b5debd7a4aa
yz <1540032876@qq.com>
2021-03-28 12:48:19 +0800

    2021.3.28 最新1 yz

--- src/main.js
+++ src/main.js
@@ -7,15 +7,11 @@ import less from 'less'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import VueParticles from 'vue-particles'
-//安装ant-design UI库
-import 'ant-design-vue/dist/antd.css'
-import Antd from 'ant-design-vue'
 
 Vue.config.productionTip = false
 Vue.use(less)
 Vue.use(ElementUI)
 Vue.use(VueParticles)
-Vue.use(Antd)
 
 new Vue({
     router,
