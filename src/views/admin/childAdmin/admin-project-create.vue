739531cbcab6338696f6d416615f14efc32b5332
yzcode <1540032876@qq.com>
2021-06-26 09:45:10 +0000

    !16 创建项目 * 'feat_project_create'

--- src/views/admin/childAdmin/admin-project-create.vue
+++ src/views/admin/childAdmin/admin-project-create.vue
@@ -1,15 +1,36 @@
 <template>
-  <div>
-    创建项目
+  <div class="project-create">
+    <el-input v-model="projectName" placeholder="请输入项目名称"></el-input>
+    <el-button @click="createProject">创建项目</el-button>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "adminProjectCreate"
-    }
+import { debounce } from "lodash";
+export default {
+  name: "adminProjectCreate",
+  data() {
+    return {
+      projectName: "",
+    };
+  },
+  methods: {
+    createProject: debounce(function() {
+      if (this.projectName === "") {
+        this.$message.error("项目名不能为空");
+        return;
+      }
+      //TODO 项目创建
+      console.log(this.projectName);
+    }, 300),
+  },
+};
 </script>
 
 <style scoped>
-
+.project-create {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
 </style>
