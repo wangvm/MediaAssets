1405121f96c72ebce65a6933ae8a8f97572daea2
yzcode <1540032876@qq.com>
2021-06-26 05:41:47 +0000

    !13 vue.config.js优化（加载速度从1100mm提升至200+mm） * Merge branch 'develop' of gitee.com:yzcode1/media-assets into fix_conf… * '优化vue.config.js'

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,85 @@
 const path = require("path");
+// 是否为生产环境
+const isProduction = process.env.NODE_ENV !== 'development';
+// gzip压缩
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
+
+const cdn = {
+  productionSourceMap: false, // 关闭生产环境的 source map
+  // 忽略打包的第三方库
+  externals: {
+    vue: 'Vue',
+    vuex: 'Vuex',
+    'vue-router': 'VueRouter',
+    axios: 'axios',
+  },
+  // 通过cdn引用
+  js: [
+    'https://cdn.bootcss.com/vue/2.6.11/vue.runtime.min.js',
+    'https://cdn.bootcss.com/vue-router/3.1.2/vue-router.min.js',
+    'https://cdn.bootcss.com/vuex/3.1.2/vuex.min.js',
+    'https://cdn.bootcss.com/axios/0.19.2/axios.min.js',
+  ],
+  css: []
+}
+
 module.exports = {
+  publicPath: '../', // 基本路径
+  outputDir: 'dist', // 输出文件目录
+  assetsDir: 'static',
+  indexPath: './public/index.html',
   chainWebpack: config => {
     config.resolve.alias
       .set("@", resolve("src"))
+    // 配置cdn引入
+    config.plugin('html').tap((args) => {
+      args[0].cdn = cdn;
+      return args;
+    });
+  },
+  configureWebpack: (config) => {
+    // 忽略打包配置
+    config.externals = cdn.externals;
+    // 生产环境相关配置
+    if (isProduction) {
+      //gzip压缩
+      const productionGzipExtensions = ['html', 'js', 'css'];
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+          threshold: 10240, // 只有大小大于该值的资源会被处理 10240
+          minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+          deleteOriginalAssets: false, // 删除原文件
+        })
+      )
+      // 公共代码抽离
+      config.optimization = {
+        // 分割代码块
+        splitChunks: {
+          cacheGroups: {
+            //公用模块抽离
+            common: {
+              chunks: 'initial',
+              minSize: 0, //大于0个字节
+              minChunks: 2, //抽离公共代码时，这个代码块最小被引用的次数
+            },
+            //第三方库抽离
+            vendor: {
+              priority: 1, //权重
+              test: /node_modules/,
+              chunks: 'initial',
+              minSize: 0, //大于0个字节
+              minChunks: 2, //在分割之前，这个代码块最小应该被引用的次数
+            },
+          },
+        },
+      }
+    }
   },
 }
