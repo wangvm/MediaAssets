4757c0d4de21ba28416bdd268e7944a54d9bd8a0
yz <1540032876@qq.com>
2021-06-27 16:14:46 +0800

    'css初始化样式'

--- src/App.vue
+++ src/App.vue
@@ -26,14 +26,7 @@ export default {
 </script>
 
 <style lang="less">
-* {
-  margin: 0;
-  padding: 0;
-}
-
-a {
-  text-decoration: none;
-}
+@import "~@/assets/css/common.css";
 
 .fade-enter-active,
 .fade-leave-avtive {
