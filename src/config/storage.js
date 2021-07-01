42eba98607b50df6dbb257d48991e84db87cbeeb
yz <1540032876@qq.com>
2021-07-01 23:22:13 +0800

    'chorm'

--- src/config/storage.js
+++ src/config/storage.js
@@ -41,8 +41,31 @@ export function removeUserToken() {
 	localStorage.removeItem(USER_TOKEN)
 }
 
-export function removeAllSessionStorage() {
-	removeLoginType()
-	removeCurrentMenu()
-	removeUserToken()
+// 判断登录状态
+export function setIfLogin(bool) {
+	localStorage.setItem(IF_LOGIN, bool)
+}
+
+export function getIfLogin() {
+	return localStorage.getItem(IF_LOGIN)
+}
+
+export function removeIfLogin() {
+	localStorage.removeItem(IFLOGIN)
+}
+N
+localStorage.removeItem(IF_LOGIN)
+}
+
+// 保存当前菜单值
+export function setCurrentMenu(currentMenu) {
+	localStorage.setItem(CURRENT_MENU, currentMenu)
+}
+
+export function getCurrentMenu() {
+	return localStorage.getItem(CURRENT_MENU)
+}
+
+export function removeCurrentMenu() {
+	localStorage.removeItem(CURRENT_MENU)
 }
